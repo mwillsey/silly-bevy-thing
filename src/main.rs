d626621085d240f0f29a924f8e38553a327c7523
darzu <daryl.zuniga@gmail.com>
2021-02-14 08:25:54 -0800

    minifying

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{borrow::Borrow, collections::HashMap};
-
 use bevy::{math::vec2, prelude::*, render::camera::Camera};
 use bevy_rapier2d::rapier::{geometry::{Collider, ColliderSet}, na::Vector2};
 use bevy_rapier2d::rapier::{dynamics::*, geometry::ColliderBuilder};
@@ -8,15 +7,14 @@ use bevy_rapier2d::{
     rapier::geometry::{ColliderHandle, ContactEvent, InteractionGroups, NarrowPhase},
 };
 
+// types
 #[derive(Default)]
-struct Blob {
-    going_right: bool
-}
+struct Blob;
 struct Platform;
-
-const ALL_GRP: u16 = u16::MAX;
-const SCALE: f32 = 20.0;
-
+#[derive(Default)]
+struct Velocity(Vec2);
+#[derive(Default)]
+struct Force(Vec2);
 #[derive(Bundle)]
 struct BoxBundle {
     sprite_bundle: SpriteBundle,
@@ -24,6 +22,50 @@ struct BoxBundle {
     col: ColliderBuilder,
 }
 
+// constants
+const ALL_GRP: u16 = u16::MAX;
+const RAPIER_SCALE: f32 = 20.0;
+
+// PHYSICS BUG
+fn blob_move(
+    time: Res<Time>,
+    narrow_phase: Res<NarrowPhase>,
+    colliders: ResMut<ColliderSet>,
+    mut rigid_bodies: ResMut<RigidBodySet>,
+    mut blobs: Query<(&mut Blob, &RigidBodyHandleComponent, &ColliderHandleComponent)>,
+    platforms: Query<&RigidBodyHandleComponent, With<Platform>>,
+) {
+    for (blob, blob_rbh, blob_cth) in blobs.iter_mut() {
+        let blob_cth = blob_cth.handle();
+        if let Some(contacting) = narrow_phase.contacts_with(blob_cth) {
+            let contacting_platforms: Vec<_> = contacting.filter_map(|ct| {
+                let other_ent = c2e(&colliders[other(blob_cth, ct)]);
+                platforms.get(other_ent).ok()
+            }).collect();
+            if contacting_platforms.len() == 1 {
+                let blob_rb = &mut rigid_bodies[blob_rbh.handle()];
+                if !blob_rb.is_moving() {
+                    // ---
+                    // CRASH! This line. Comment out to hide crash
+                    blob_rb.apply_impulse([100.0, 100.0].into(), true);
+                    // CRASH! This line. Comment out to hide crash
+                    // ---
+                }
+            }
+        }
+    }
+}
+
+fn other<T>(me: ColliderHandle, contact: (ColliderHandle, ColliderHandle, T)) -> ColliderHandle {
+    if me == contact.0 {
+        contact.1
+    } else {
+        assert_eq!(me, contact.1);
+        contact.0
+    }
+}
+
+// GAME SETUP
 fn spawn_box<'a>(
     cmd: &'a mut Commands,
     material: Handle<ColorMaterial>,
@@ -47,10 +89,10 @@ fn spawn_box<'a>(
     } else {
         RigidBodyBuilder::new_static()
     }
-    .translation(x / SCALE, y / SCALE);
+    .translation(x / RAPIER_SCALE, y / RAPIER_SCALE);
     let rb = rig_cb(rb);
     let col =
-        ColliderBuilder::cuboid(w / 2.0 / SCALE, h / 2.0 / SCALE)
+        ColliderBuilder::cuboid(w / 2.0 / RAPIER_SCALE, h / 2.0 / RAPIER_SCALE)
           .user_data(ent.to_bits() as u128);
     let col = col_cb(col);
     cmd.with(rb).with(col)
@@ -92,7 +134,7 @@ fn setup_game(
     println!("Hello world!");
 
     // global settings
-    rapier_config.scale = SCALE;
+    rapier_config.scale = RAPIER_SCALE;
     rapier_config.gravity = Vector2::new(0.0, -100.0);
 
     // camera
@@ -122,48 +164,6 @@ fn setup_game(
     // spawn blob?
     spawn_blob(commands, &mut materials, 0.0, 5.0, 0.5);
 }
-
-fn other<T>(me: ColliderHandle, contact: (ColliderHandle, ColliderHandle, T)) -> ColliderHandle {
-    if me == contact.0 {
-        contact.1
-    } else {
-        assert_eq!(me, contact.1);
-        contact.0
-    }
-}
-
-fn blob_move(
-    time: Res<Time>,
-    narrow_phase: Res<NarrowPhase>,
-    colliders: ResMut<ColliderSet>,
-    mut rigid_bodies: ResMut<RigidBodySet>,
-    mut blobs: Query<(&mut Blob, &RigidBodyHandleComponent, &ColliderHandleComponent)>,
-    platforms: Query<&RigidBodyHandleComponent, With<Platform>>,
-) {
-    for (blob, blob_rbh, blob_cth) in blobs.iter_mut() {
-        let blob_cth = blob_cth.handle();
-        if let Some(contacting) = narrow_phase.contacts_with(blob_cth) {
-            let contacting_platforms: Vec<_> = contacting.filter_map(|ct| {
-                let other_ent = c2e(&colliders[other(blob_cth, ct)]);
-                platforms.get(other_ent).ok()
-            }).collect();
-            if contacting_platforms.len() == 1 {
-                let blob_rb = &mut rigid_bodies[blob_rbh.handle()];
-                if !blob_rb.is_moving() {
-                    // TODO:
-                    // blob_rb.apply_impulse([100.0, 100.0].into(), true);
-                }
-                // let platform_rb = &rigid_bodies[contacting_platforms[0].handle()];
-            }
-        }
-    }
-}
-
-#[derive(Default)]
-struct Velocity(Vec2);
-
-#[derive(Default)]
-struct Force(Vec2);
 
 fn main() {
     App::build()
